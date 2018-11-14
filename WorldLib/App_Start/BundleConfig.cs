a6fc266310f66fa11269ac753dd667566c79dd9c
рома ершов <energy26622662@outlook.com>
2018-11-15 00:11:23 +0600

    стили для карт и ховер

--- WorldLib/App_Start/BundleConfig.cs
+++ WorldLib/App_Start/BundleConfig.cs
@@ -21,6 +21,7 @@ namespace WorldLib
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.min.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
