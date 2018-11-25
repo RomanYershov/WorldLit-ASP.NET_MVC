9cd005f32ce31bb69f9f9a33450874173ae3a4d6
рома ершов <energy26622662@outlook.com>
2018-11-25 21:11:15 +0600

    foodCategory CRUD/for admin

--- WorldLib/Scripts/models/food-category-view-model.js
+++ WorldLib/Scripts/models/food-category-view-model.js
@@ -12,27 +12,28 @@
         target.subscribe(validate);
         return target;
     }
-   
+
     var Category = function (id, name, recipes) {
         this.id = id;
-        this.name = name;
+        this.name = ko.observable(name);
         this.recipes = recipes;
+        this.isEdit = ko.observable(false);
     }
     self.recipes = ko.observableArray([]);
     self.foodCategories = ko.observableArray([]);
 
     self.isAddCategory = ko.observable(false);
-    self.showCategoryForm = function() {
+    self.showCategoryForm = function () {
         self.isAddCategory(true);
     };
 
-    self.newCategoryName = ko.observable(null).extend({required: "Необходимо ввести название .."});
-    self.addCategory = function(formElement) {
+    self.newCategoryName = ko.observable(null).extend({ required: "Необходимо ввести название .." });
+    self.addCategory = function (formElement) {
         if (self.newCategoryName() == null) return false;
         if (self.newCategoryName().length == 0) return false;
         $.post("/Admin/CreateCategory",
             { name: self.newCategoryName() },
-            function(result) {
+            function (result) {
                 if (result != null) {
                     self.foodCategories.unshift(new Category(result.Id, result.Name, result.Recipes));
                     self.isAddCategory(false);
@@ -41,31 +42,37 @@
             });
     };
 
-    self.deleteCategory = function(category) {
+    self.deleteCategory = function (category) {
         $.post("/Admin/deleteCategory",
             { id: category.id },
-            function(res) {
+            function (res) {
                 if (res === "success") {
                     self.foodCategories.remove(category);
                 }
             });
     };
 
-    self.editCategory = function(category) {
+
+    self.editCategory = function (category) {
         debugger;
-        $.post("/Admin/editCategory",
-            {id: id, name: name},
-            function(res) {
-                debugger;
-            });
+        if (category.isEdit()) {
+            $.post("/Admin/editCategory",
+                { id: category.id, name: category.name },
+                function (res) {
+                    category.isEdit(false);
+                });
+        }
+        category.isEdit(true);
     };
+    self.editCancel = function(category) {
+        category.isEdit(false);
+    }
 
-
-    self.getRecipes = function () {
+    self.getCategories = function () {
         $.get("/recipe/getCategories",
             function (data) {
                 for (var i = 0; i < data.length; i++) {
-                    self.foodCategories.push(new Category(data[i].Id, data[i].Name,data[i].Recipes));
+                    self.foodCategories.push(new Category(data[i].Id, data[i].Name, data[i].Recipes));
                 }
                 self.getAllRecipes();
             });
@@ -96,14 +103,14 @@
                     image: value.recipes[i].ImageUrl,
                     isClickRecipe: ko.observable(false),
                     recipeComments: value.recipes[i].RecipeComments
-            });
+                });
             }
         });
 
     }
-    self.getRecipes();
+    self.getCategories();
 
-   
+
     self.search = ko.observable();
     self.search.subscribe(function (value) {
         self.recipes([]);
@@ -124,9 +131,8 @@
             }
         });
     });
- 
+
     self.removeBlock = function (data) {
-        debugger;
         data.isClickRecipe(false);
     }
 }
